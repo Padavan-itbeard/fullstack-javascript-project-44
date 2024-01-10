c63434629a12b8e60e8440568f4ecfbb16842c93
Alexandr Ivanov <Alexandr.ivanov.1024@gmail.com>
2024-01-10 22:55:32 +0300

    add greeting

---
+++ src/cli.js
@@ -0,0 +1,9 @@
+import readlineSync from 'readline-sync';
+
+function greeting() {
+  const name = readlineSync.question('May I have your name? ');
+
+  console.log(`Hello, ${name}!`);
+}
+
+export { greeting };
